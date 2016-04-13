59e39d71deb89b231d9350b9e2777925897f7677
wannabeCitizen <doorsofskirpan@gmail.com>
2016-04-13 14:19:01 -0600

    added socket stuff

--- static/ownup.js
+++ static/ownup.js
@@ -3,6 +3,10 @@ var totalPlayers;
 var activeButtons = [false, false, false, false];
 var myGame;
 var placements;
+var socket = io.connect('http://localhost:3000');
+socket.on('rfid', function (data) {
+    setTimeout(function () {window.location = "http://localhost:8000"}, 2000)
+});
 
 
 //Full Game
